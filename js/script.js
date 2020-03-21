09476b1e398a5b0c764df47b181ce388e74918a7
Neohack69 <57541227+Neohack69@users.noreply.github.com>
2020-03-21 20:38:42 +0300

    Завершение проекта. Исправление 2.

--- js/script.js
+++ js/script.js
@@ -9,15 +9,3 @@ close.addEventListener("click", function () {
   var form = document.querySelector(".feedback");
   form.classList.add("visuallyHidden");
 });
-
-var popup = document.querySelector(".location");
-popup.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  var map = document.querySelector(".popupWindow");
-  map.classList.remove("visuallyHidden");
-});
-var mapclose = document.querySelector(".popupClose");
-mapclose.addEventListener("click", function () {
-  var close = document.querySelector(".popupWindow");
-  close.classList.add("visuallyHidden");
-});
