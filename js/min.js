4ab0c14b5e2fc44667da7ba2b3700ee22d68e56e
Neohack69 <57541227+Neohack69@users.noreply.github.com>
2020-03-22 13:06:32 +0300

    Завершение проекта. Исправление 4.

---
+++ js/min.js
@@ -0,0 +1,1 @@
+var link=document.querySelector("#formOpen");link.addEventListener("click",function(evt){evt.preventDefault();var form=document.querySelector(".feedback");form.classList.remove("visuallyHidden")});var close=document.querySelector(".modalClose");close.addEventListener("click",function(){var form=document.querySelector(".feedback");form.classList.add("visuallyHidden")})
